ca7be254580654842162dc24e0b363a2d7e17e81
guiteste23 <52545001+guiteste23@users.noreply.github.com>
2019-07-07 14:41:20 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var n1 = Number(window.prompt("Insira um numero?"))
-var n2 = Number(window.prompt("Insira um numero?"))
-var result = `= ${n1 + n2}`
-window.alert(`A soma dos valores ${n1} e ${n2} é ${result}`)
-var nome = window.prompt("Qual seu nome?")
-window.alert(`seu nome tem ${nome.length} letras`)
-window.alert(`Veja agora seu nome em letras maiusculas ${nome.toUpperCase()}`)
